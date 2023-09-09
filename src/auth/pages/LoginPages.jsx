9208b29ae8363b87e5843a271e938a9e70825c2b
andresGarcia90 <garcia.andi90@gmail.com>
2023-09-09 19:35:51 -0300

    Adding search page

--- src/auth/pages/LoginPages.jsx
+++ src/auth/pages/LoginPages.jsx
@@ -3,14 +3,14 @@ import { useNavigate } from 'react-router-dom'
 
 export const LoginPages = () => {
   const navigate = useNavigate();
-  const onLoginHandle = () => {
+  const onLogin = () => {
     navigate('/marvel')
   };
   return (
     <div className="container mt-5">
       <h1>Login</h1>
       <hr />
-      <button className="btn btn-primary" onClick={onLoginHandle}>
+      <button className="btn btn-primary" onClick={onLogin}>
         Login
       </button>
     </div>
